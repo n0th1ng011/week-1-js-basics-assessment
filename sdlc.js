abd4a426c2dbbfd30d68db3ad90017cd6e1e392f
Po <po@Pos-MacBook-Pro.local>
2022-06-17 14:06:45 -0600

    Inital Commit

---
+++ sdlc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+//Planning
+//Analysis of Requirements
+//Design
+//Implemention
+//Testing and Integration
+//Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This step has the developer assess what resources they will need to develop their software. This can range anywhere from the coding languages and software used, to the hardware it will run on. "
+const design = "Where whiteboarding and pseudocode come into play. Before devlopers begin to code, they write out in plain english (or whichever language they speak) what functions are needed to achieve the desired effect in their apllication or software."
+const implementation = "The actual coding process. While it's possible to have one developer working on a small/personal project, it's better to have a team of developers working on lager scope projects to help avoid wiriting in bugs/errors and help improve teamwork and team members less used skills."
+const testingandintegration = "Before releasing their software, developers test their code to see if its working properly in their pre-defined requirements. For instance, if the software was designed to run on both Mac and Windows systems and while testing it chrashes on MacOS, there's a problem. After working out issues as big as compatibilty and as small as performance tweaks, the software can be released or integrated to a pre-existing project."
+const maintain = "The end of the cycle and the beginning of a new one. End-users report bugs that weren't discovered in testing, then developers start back at planning to begin the process of removing said bugs and maintaining the software."
